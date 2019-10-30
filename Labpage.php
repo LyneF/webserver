1f68de06a3da2a8c90389864b72dbdc32eb5d2d6
LyneF <lyne.fotchin@outlook.com>
2019-10-30 15:31:42 -0400

    Added php script to index.php and turned all html pages into php. Modified Labpage.php so it sends data to index.php.

--- Labpage.php
+++ Labpage.php
@@ -1,49 +1,65 @@
 <!DOCTYPE html>
 
 <html>
-    <heaD>
+    <head>
     	<center><img src="IMG/Dawson.png" alt="Dawson logo" width="300" height="150"/></center>
 		<center><h1>Electronics Engineering Technology<br/>243-544-DW</h1></center>
-        <hr>
+        <hr/>
     </head>
     <body bgcolor="#158DEA" style="font-family:agency fb;">
         <h2>Customized title page</h2>
         <p>Please fill in the following information in order to create your personal title page.</p>
-		
-        <form>
+	<style>
+        .button {
+                background-color: black;
+                border: none;
+                color: #158DEA;
+                padding: 20px 30px;
+                text-align: center;
+                font-size: 20px;
+                cursor: pointer;
+                style="font-family:agency fb;"
+                }
+
+        .button:hover {
+                background-color: black;
+                color: #EEEEEE;
+                style="font-family:agency fb;"
+                }
+        </style>
+	
+        <form action="index.php" method="post">
         	Course name:<br>
         	<input type="text" name="coursename"><br>
         	Course number:<br>
         	<input type="text" name="coursenum"><br><br>
-	</form>
-        
-        <form>
+
         	Lab number:<br>
         	<input type="text" name="labnum"><br>
         	Lab name:<br>
         	<input type="text" name="labname"><br><br>
-	</form>
-        
-        <form>
+
 		1rst name:<br>
-		<input type="text" name="firstname"><br>
+		<input type="text" name="name1"><br>
 		2nd name:<br>
-		<input type="text" name="secname"><br>
+		<input type="text" name="name2"><br>
 		Student ID 1:<br>
-		<input type="text" name="studentid1"><br>
+		<input type="text" name="id1"><br>
 		Student ID 2:<br>
-		<input type="text" name="studentid2"><br><br>
-	</form>
-        
-	<form>
+		<input type="text" name="id2"><br><br>
+
 		Date of experiment:<br>
-          	<input type="text" name="dateexp"><br>
+          	<input type="text" name="date1"><br>
           	Date submitted:<br>
-          	<input type="text" name="datesubm"><br>
+          	<input type="text" name="date2"><br>
 	  	Teacher name:<br>
-          	<input type="text" name="teachname"><br>
+          	<input type="text" name="teacher"><br>
+
+		<br>
+		<input class="button" type="submit"/>
 	</form>
-		<p>The rest of this page will soon come. Expect more details. See you.</p>
+	
+	<p>The rest of this page will soon come. Expect more details. See you.</p>
 		
 	<h2>
 		If you're lost, click <a href="index.php">here</a>
@@ -52,3 +68,4 @@
     </body>
 
 </html>
+	
