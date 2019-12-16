0fa1532be64c4dfc550e5db0543578b95f7e5c67
LyneF <55206774+LyneF@users.noreply.github.com>
2019-12-16 12:09:02 -0500

    Update g00gle.php

--- g00gle.php
+++ g00gle.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
 
+<body>
+    <center>
+			<img src="g00gie.php/googlelogocolor_272×92dp.png" alt="Dawson logo" width="300" height="150"/>
+    </center>
+    <br/>
+    
+    <form action="Lab.php" method="post">
+        <input type="text" name="coursename">
+    </form>    
+
+
+</body>
+
+
 <?php
 
 ?>
